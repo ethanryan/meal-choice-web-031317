b547ac7d4b47be624080189aa1a69ff41229b054
Ethan Ryan <ethanryan@gmail.com>
2017-02-23 16:31:54 +0000

    Done.

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,12 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
+
+def meal_choice(meal="meat") #meal is argument, default argument is "meat"
+  "#{meal}" #method returns #{meal}
+end #end method
+
+
+#this also works, but need to say 'return'
+# def meal_choice(meal="meat") #meal is argument, default argument is "meat"
+#   return meal #method returns #{meal}
+# end #end method
